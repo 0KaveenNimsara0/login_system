4287bed5e21302846c60d0dc0b1cc0ffa961d89e
Kaveen Nimsara <138862572+0KaveenNimsara0@users.noreply.github.com>
2026-09-21 18:35:31 +0530

    feat: add routing and HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import LoginPage from './pages/LoginPage';
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import TokenPage from "./pages/TokenPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/token" element={<TokenPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
